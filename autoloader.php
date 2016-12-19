ce8a2cc3cda4b2ba07e483f2cc618b58dfa1ef7d
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2016-12-19 16:10:01 -0500

    Added the autoloader

---
+++ autoloader.php
@@ -0,0 +1,12 @@
+<?php
+  class Autoloader{
+    public static function loader($class){
+      $filename = strtolower($class) . '.php';
+      $file = 'classes/' . $filename;
+      if (!file_exists($file)){
+        return false;
+      }
+      include $file;
+    }
+  }
+?>

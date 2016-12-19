6040e286ec70cabda26cd429e0b70cad47d6600f
Tameem Haj-Ibrahim <tameemhajibrahim@gmail.com>
2016-12-19 16:08:46 -0500

    Added the registration form code

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php require('includes/config.php');
+
+//if logged in redirect to members page
+if( $user->is_logged_in() ){ header('Location: memberpage.php'); }
+
+//if form has been submitted process it
+if(isset($_POST['submit'])){
+
+  $v = new Validator;
+  $error = $v->validate($db);
+
+
+	//if no errors have been created carry on
+	if(!isset($error)){
+
+		//hash the password
+		$hashedpassword = $user->password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+		//create the activation code
+		$activation = md5(uniqid(rand(),true));
+
+		try {
+
+			//insert into database with a prepared statement
+			$stmt = $db->prepare('INSERT INTO members (username,password,email,active) VALUES (:username, :password, :email, :active)');
+			$stmt->execute(array(
+				':username' => $_POST['username'],
+				':password' => $hashedpassword,
+				':email' => $_POST['email'],
+				':active' => $activation
+			));
+			$id = $db->lastInsertId('memberID');
+
+			//send email
+			$to = $_POST['email'];
+			$subject = "Registration Confirmation";
+			$body = "<p>Thank you for registering at demo site.</p>
+			<p>To activate your account, please click on this link: <a href='".DIR."activate.php?x=$id&y=$activation'>".DIR."activate.php?x=$id&y=$activation</a></p>
+			<p>Regards Site Admin</p>";
+
+			$mail = new Mail();
+			$mail->setFrom(SITEEMAIL);
+			$mail->addAddress($to);
+			$mail->subject($subject);
+			$mail->body($body);
+			$mail->send();
+
+			//redirect to index page
+			header('Location: index.php?action=joined');
+			exit;
+
+		//else catch the exception and show the error.
+		} catch(PDOException $e) {
+		    $error[] = $e->getMessage();
+		}
+
+	}
+
+}
+
+//define page title
+$title = 'Registration';
+
+//include header template
+require('layout/header.php');
+?>
+
+
+<div class="container">
+
+	<div class="row">
+
+	    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+			<form role="form" method="post" action="" autocomplete="off">
+				<h2>Please Sign Up</h2>
+				<p>Already a member? <a href='login.php'>Login</a>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;View current members: <a href='database.php'>Database</a></p>
+				<hr>
+
+				<?php
+				//check for any errors
+				if(isset($error)){
+					foreach($error as $error){
+						echo '<p class="bg-danger">'.$error.'</p>';
+					}
+				}
+
+				//if action is joined show sucess
+				if(isset($_GET['action']) && $_GET['action'] == 'joined'){
+					echo "<h2 class='bg-success'>Registration successful, please check your email to activate your account.</h2>";
+				}
+				?>
+
+				<div class="form-group">
+					<input type="text" name="username" id="username" class="form-control input-lg" placeholder="User Name" value="<?php if(isset($error)){ echo $_POST['username']; } ?>" tabindex="1">
+				</div>
+				<div class="form-group">
+					<input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value="<?php if(isset($error)){ echo $_POST['email']; } ?>" tabindex="2">
+				</div>
+				<div class="row">
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<div class="form-group">
+							<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="3">
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<div class="form-group">
+							<input type="password" name="passwordConfirm" id="passwordConfirm" class="form-control input-lg" placeholder="Confirm Password" tabindex="4">
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-xs-6 col-md-6"><input type="submit" name="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="5"></div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+</div>
+
+<?php
+//include header template
+require('layout/footer.php');
+?>
